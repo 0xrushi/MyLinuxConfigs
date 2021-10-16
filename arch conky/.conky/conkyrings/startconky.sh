e55186604eeba2b8992bd0030e66a4a218cc381d
rushic24 <rushic24@gmail.com>
2021-10-16 08:45:32 -0400

    added firefox profie

--- arch conky/.conky/conkyrings/startconky.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-conky -c cpu &
-conky -c mem &
-sleep 5 #time for the main conky to start; needed so that the smaller ones draw above not below (probably can be lower, but we still have to wait 5s for the rings to avoid segfaults)
-conky -c rings & # the main conky with rings
